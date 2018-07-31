05abb50e4ff3869cc9142f959f60304e3d0edc9f
Dmitriy Korovaev <kda.biz83@gmail.com>
2018-07-31 13:48:14 +0300

    -

--- x10project/telegrambot.py
+++ x10project/telegrambot.py
@@ -16,14 +16,14 @@ class BotHelper:
 
         echo_handler = MessageHandler(Filters.text, self._echoCommand)
         start_handler = CommandHandler('start', self._startCommand)
-        caps_handler = CommandHandler('caps', self._capsCommand, pass_args=True)
+        help_handler = CommandHandler('help', self._helpCommand)
         account_handler = CommandHandler('acc', self._accCommand, pass_args=True)
         unknown_handler = MessageHandler(Filters.command, self._unknownCommand)
 
 
         self.dispatcher.add_handler(start_handler)
         self.dispatcher.add_handler(echo_handler)
-        self.dispatcher.add_handler(caps_handler)
+        self.dispatcher.add_handler(help_handler)
         self.dispatcher.add_handler(account_handler)
         self.dispatcher.add_handler(unknown_handler)
 
@@ -38,10 +38,11 @@ class BotHelper:
         bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
     #обработчик команды /caps    
-    def _capsCommand(self, bot, update, args):
-        text_caps = ' '.join(args).upper()
-        bot.send_message(chat_id=update.message.chat_id, text=text_caps)
+    def _helpCommand(self, bot, update):
+        text_commands = '1. /acc [{:s}] - Выводит информацию по заданному аккаунту \n'.format(', '.join(self.availAcc))
+        bot.send_message(chat_id=update.message.chat_id, text=text_commands)
 
+    
     #обработчик команды /acc    
     def _accCommand(self, bot, update, args):
         msg = '' if len(args) == 1 and args[0] in self.availAcc else "Для выполнения команды мне нужен аргумент. Одно из следующих значений: " + ', '.join(self.availAcc)
@@ -56,9 +57,6 @@ class BotHelper:
     def _unknownCommand(self, bot, update):
         bot.send_message(chat_id=update.message.chat_id, text="Я не знаю этой команды😔")
     
-    def test(self, acc_code):
-        print(self.accCreator.getAccount(acc_code).getCommonAccountInfo())
-        
     #Запуск слушателя команд бота    
     def start(self): 
         self.updater.start_polling()
